49ee4864b8ae09325fd5b19ab349fa3ec2af8b9b
Rachel Gibbons <rachelgibbons@Rachels-MacBook-Pro-3.local>
2017-05-04 16:27:28 -0400

    100 thoughts

---
+++ exercise-5/js/shuffle.js
@@ -0,0 +1,76 @@
+  $(document).ready(function() {
+  
+    //mouse interaction
+      
+      $('.set-1').mouseover(function(){
+        $(this).css('color','#FEDD00');
+      });
+      $('.set-1').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+       $('.set-2').mouseover(function(){
+        $(this).css('color','#FE5000');
+      });
+      $('.set-2').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+       $('.set-3').mouseover(function(){
+        $(this).css('color','#CE0058');
+      });
+      $('.set-3').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+       $('.set-4').mouseover(function(){
+        $(this).css('color','#E10098');
+      });
+      $('.set-4').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+       $('.set-5').mouseover(function(){
+        $(this).css('color','#BB29BB');
+      });
+      $('.set-5').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+      $('.set-6').mouseover(function(){
+        $(this).css('color','#440099');
+      });
+      $('.set-6').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+      $('.set-7').mouseover(function(){
+        $(this).css('color','#10069F');
+      });
+      $('.set-7').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+      $('.set-8').mouseover(function(){
+        $(this).css('color','#009A44');
+      });
+      $('.set-8').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+      $('.set-9').mouseover(function(){
+        $(this).css('color','#97D700');
+      });
+      $('.set-9').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+      $('.set-10').mouseover(function(){
+        $(this).css('color','#CEDC00');
+      });
+      $('.set-10').mouseout(function(){
+        $(this).css('color','white');
+      });
+      
+      
+  });
