9bb7112de5e2b0bfcca7bde5b27e13474b535119
Rachel Gibbons <rachelgibbons@Rachels-MBP-3.wireless.newschool.edu>
2017-05-11 18:47:04 -0400

    updated exercises

--- exercise-5/js/shuffle.js
+++ exercise-5/js/shuffle.js
@@ -3,70 +3,70 @@
     //mouse interaction
       
       $('.set-1').mouseover(function(){
-        $(this).css('color','#FEDD00');
+        $(this).css('color','#207F49');
       });
       $('.set-1').mouseout(function(){
         $(this).css('color','white');
       });
       
        $('.set-2').mouseover(function(){
-        $(this).css('color','#FE5000');
+        $(this).css('color','#46D3E8');
       });
       $('.set-2').mouseout(function(){
         $(this).css('color','white');
       });
       
        $('.set-3').mouseover(function(){
-        $(this).css('color','#CE0058');
+        $(this).css('color','#5A7DFF');
       });
       $('.set-3').mouseout(function(){
         $(this).css('color','white');
       });
       
        $('.set-4').mouseover(function(){
-        $(this).css('color','#E10098');
+        $(this).css('color','#9046E8');
       });
       $('.set-4').mouseout(function(){
         $(this).css('color','white');
       });
       
        $('.set-5').mouseover(function(){
-        $(this).css('color','#BB29BB');
+        $(this).css('color','#FF4DD2');
       });
       $('.set-5').mouseout(function(){
         $(this).css('color','white');
       });
       
       $('.set-6').mouseover(function(){
-        $(this).css('color','#440099');
+        $(this).css('color','#57D917');
       });
       $('.set-6').mouseout(function(){
         $(this).css('color','white');
       });
       
       $('.set-7').mouseover(function(){
-        $(this).css('color','#10069F');
+        $(this).css('color','#E86619');
       });
       $('.set-7').mouseout(function(){
         $(this).css('color','white');
       });
       
       $('.set-8').mouseover(function(){
-        $(this).css('color','#009A44');
+        $(this).css('color','#FFBC28');
       });
       $('.set-8').mouseout(function(){
         $(this).css('color','white');
       });
       
       $('.set-9').mouseover(function(){
-        $(this).css('color','#97D700');
+        $(this).css('color','#E8DC19');
       });
       $('.set-9').mouseout(function(){
         $(this).css('color','white');
       });
       
       $('.set-10').mouseover(function(){
-        $(this).css('color','#CEDC00');
+        $(this).css('color','#57D917');
       });
       $('.set-10').mouseout(function(){
         $(this).css('color','white');
